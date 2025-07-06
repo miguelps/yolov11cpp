33957b17f85b7f4ec219b33cf0ff99fc27f748b2
Miguel Pari Soto <miguel@idwall.co>
2025-07-06 01:07:16 -0300

    feat(chore): tst

--- main.cpp
+++ main.cpp
@@ -2,50 +2,60 @@
 #include <onnxruntime_cxx_api.h>
 
 #define MAX_STRIDE 32
+#define TARGET_SIZE 640
 
-struct Object {
+struct Object
+{
     cv::Rect_<float> rect;
     int label;
     float prob;
 };
 
-std::string getInputName(Ort::Session& session) {
+std::string getInputName(Ort::Session &session)
+{
     Ort::AllocatorWithDefaultOptions allocator;
     Ort::AllocatedStringPtr name_allocator = session.GetInputNameAllocated(0, allocator);
     return std::string(name_allocator.get());
 }
 
-std::string getOutputName(Ort::Session& session) {
+std::string getOutputName(Ort::Session &session)
+{
     Ort::AllocatorWithDefaultOptions allocator;
     Ort::AllocatedStringPtr name_allocator = session.GetOutputNameAllocated(0, allocator);
     return std::string(name_allocator.get());
 }
 
-float clamp(float val, float min = 0.f, float max = 1280.f) {
+float clamp(float val, float min = 0.f, float max = 1280.f)
+{
     return std::max(min, std::min(max, val));
 }
 
 void non_max_suppression(
-    std::vector<Object>& proposals, std::vector<Object>& results,
-    int orin_h, int orin_w, float conf_thres = 0.25f, float iou_thres = 0.65f
-) {
+    std::vector<Object> &proposals, std::vector<Object> &results,
+    int orin_h, int orin_w, float conf_thres = 0.25f, float iou_thres = 0.65f)
+{
     std::vector<cv::Rect> bboxes;
     std::vector<float> scores;
     std::vector<int> indices;
 
-    for (const auto& obj : proposals) {
+    for (const auto &obj : proposals)
+    {
         bboxes.push_back(obj.rect);
         scores.push_back(obj.prob);
     }
 
     cv::dnn::NMSBoxes(bboxes, scores, conf_thres, iou_thres, indices);
 
-    for (int i : indices) {
+    for (int i : indices)
+    {
         results.push_back(proposals[i]);
     }
 }
 
-void preprocess(const cv::Mat& img, cv::Mat& out, int target_size = 640) {
+void preprocess(const cv::Mat &img, std::vector<float> &input_tensor, int target_size = TARGET_SIZE)
+{
+    cv::Mat resized, input;
+
     int img_w = img.cols;
     int img_h = img.rows;
 
@@ -53,7 +63,6 @@ void preprocess(const cv::Mat& img, cv::Mat& out, int target_size = 640) {
     int new_w = img_w * scale;
     int new_h = img_h * scale;
 
-    cv::Mat resized;
     cv::resize(img, resized, cv::Size(new_w, new_h));
 
     int top = (target_size - new_h) / 2;
@@ -61,15 +70,24 @@ void preprocess(const cv::Mat& img, cv::Mat& out, int target_size = 640) {
     int left = (target_size - new_w) / 2;
     int right = target_size - new_w - left;
 
-    cv::copyMakeBorder(resized, out, top, bottom, left, right, cv::BORDER_CONSTANT, cv::Scalar(114, 114, 114));
-    out.convertTo(out, CV_32F, 1.0 / 255);
+    cv::copyMakeBorder(resized, input, top, bottom, left, right, cv::BORDER_CONSTANT, cv::Scalar(114, 114, 114));
+    input.convertTo(input, CV_32F, 1.0 / 255);
+
+    input_tensor.reserve(input.rows * input.cols * input.channels());
+    for (int c = 0; c < input.channels(); ++c)
+        for (int w = 0; w < input.cols; ++w)
+            for (int h = 0; h < input.rows; ++h)
+                input_tensor.push_back(input.at<cv::Vec3f>(h, w)[c]);
 }
 
-void postprocess(const float* output, int rows, int cols, float conf_threshold, std::vector<Object>& objects) {
-    for (int i = 0; i < rows; ++i) {
-        float confidence = output[i * cols + 4];  // Object confidence
+void postprocess(const float *output, int rows, int cols, float conf_threshold, std::vector<Object> &objects)
+{
+    for (int i = 0; i < rows; ++i)
+    {
+        float confidence = output[i * cols + 4]; // Object confidence
 
-        if (confidence >= conf_threshold) {
+        if (confidence >= conf_threshold)
+        {
             float x = output[i * cols];
             float y = output[i * cols + 1];
             float w = output[i * cols + 2];
@@ -86,35 +104,57 @@ void postprocess(const float* output, int rows, int cols, float conf_threshold, 
     }
 }
 
-void detect_yolov11(const cv::Mat& img, Ort::Session& session, std::vector<Object>& objects) {
-    cv::Mat input;
-    preprocess(img, input);
+void detect_yolov11(const cv::Mat &img, Ort::Session &session, int target_size, std::vector<Object> &objects)
+{
+    std::vector<float> input_tensor;
 
-    std::vector<int64_t> input_shape = {1, 3, input.rows, input.cols};
-    std::vector<float> input_tensor_values(input.begin<float>(), input.end<float>());
+    std::cout << "target_size: " << target_size << std::endl;
 
-    Ort::MemoryInfo memory_info = Ort::MemoryInfo::CreateCpu(OrtArenaAllocator, OrtMemTypeDefault);
-    Ort::Value input_tensor = Ort::Value::CreateTensor<float>(
-        memory_info, input_tensor_values.data(), input_tensor_values.size(), input_shape.data(), input_shape.size());
+    preprocess(img, input_tensor, target_size);
 
-    const char* input_name = getInputName(session).c_str();
-    const char* output_name = getOutputName(session).c_str();
+    std::vector<int64_t> input_shape = {1, 3, target_size, target_size};
 
-    auto output_tensors = session.Run(Ort::RunOptions{nullptr}, &input_name, &input_tensor, 1, &output_name, 1);
+    const char *input_name = getInputName(session).c_str();
+    const char *output_name = getOutputName(session).c_str();
 
-    float* output_data = output_tensors[0].GetTensorMutableData<float>();
+    Ort::MemoryInfo memory_info =
+        Ort::MemoryInfo::CreateCpu(
+            OrtArenaAllocator,
+            OrtMemTypeDefault);
+    Ort::Value input_tensor_ort =
+        Ort::Value::CreateTensor<float>(
+            memory_info,
+            input_tensor.data(),
+            input_tensor.size(),
+            input_shape.data(),
+            input_shape.size());
+
+    auto output_tensors =
+        session.Run(
+            Ort::RunOptions{nullptr},
+            &input_name,
+            &input_tensor_ort,
+            1,
+            &output_name,
+            1);
+
+    float *output_data = output_tensors[0].GetTensorMutableData<float>();
     auto output_shape = output_tensors[0].GetTensorTypeAndShapeInfo().GetShape();
 
     int rows = output_shape[1];
     int cols = output_shape[2];
 
-    postprocess(output_data, rows, cols, 0.25f, objects);
+    std::cout << "rows: " << rows << ", cols: " << cols << std::endl;
+
+    postprocess(output_data, rows, cols, 0.5f, objects);
 }
 
-void draw_objects(const cv::Mat& img, const std::vector<Object>& objects) {
+void draw_objects(const cv::Mat &img, const std::vector<Object> &objects)
+{
     cv::Mat image = img.clone();
 
-    for (const auto& obj : objects) {
+    for (const auto &obj : objects)
+    {
         cv::rectangle(image, obj.rect, cv::Scalar(255, 0, 0), 2);
 
         std::string label = std::to_string(obj.label) + ": " + std::to_string(int(obj.prob * 100)) + "%";
@@ -132,27 +172,36 @@ void draw_objects(const cv::Mat& img, const std::vector<Object>& objects) {
     cv::imwrite("output.jpg", image);
 }
 
-int main(int argc, char** argv) {
-    if (argc != 3) {
+int main(int argc, char **argv)
+{
+    if (argc < 3)
+    {
         std::cerr << "Usage: " << argv[0] << " <model.onnx> <imagepath>" << std::endl;
         return -1;
     }
 
     const std::string model_path = argv[1];
     const std::string image_path = argv[2];
+    int target_size = TARGET_SIZE;
+    if (argc == 4)
+    {
+        const std::string side_size = argv[3];
+        target_size = std::stoi(side_size);
+    }
+
+    cv::Mat img = cv::imread(image_path);
+    if (img.empty())
+    {
+        std::cerr << "Failed to read image: " << image_path << std::endl;
+        return -1;
+    }
 
     Ort::Env env(OrtLoggingLevel::ORT_LOGGING_LEVEL_ERROR, "YOLOv11");
     Ort::SessionOptions session_options;
     Ort::Session session(env, model_path.c_str(), session_options);
 
-    cv::Mat img = cv::imread(image_path);
-    if (img.empty()) {
-        std::cerr << "Failed to read image: " << image_path << std::endl;
-        return -1;
-    }
-
     std::vector<Object> objects;
-    detect_yolov11(img, session, objects);
+    detect_yolov11(img, session, target_size, objects);
     draw_objects(img, objects);
 
     return 0;
